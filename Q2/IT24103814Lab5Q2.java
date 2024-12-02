9a4b6998c5256c3acc814a6246801a01d4b7b220
IT24103814 <it24103814@my.sliit.lk>
2024-12-02 07:12:11 +0530

    Add files via upload

---
+++ Q2/IT24103814Lab5Q2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class IT24103814Lab5Q2
+{
+	public static void main(String[] args)
+	
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the number of new members introducted: ");
+		int members = input.nextInt();
+		
+		System.out.println();
+		
+		if (members < 0)
+		{System.out.print("Input must be a number 0 or greater");}
+	
+	    else 
+		{
+		  switch (members)
+          
+		  {
+			case 0:
+			System.out.println("No prize");
+			break;
+			
+			case 1:
+			System.out.println("Prize is a : Pen");
+			break;
+			
+			case 2:
+			System.out.println("Prize is a : Umbrella");
+			break;
+			
+			case 3:
+			System.out.println("Prize is a : Bag");
+			break;
+			
+			case 4:
+			System.out.println("Prize is a : Travelling Chair");
+            break;
+
+            default:
+            System.out.println("Prize is a : Headphone");
+            break;
+            
+		    
+		  }		  
+		}
+    }
+}
